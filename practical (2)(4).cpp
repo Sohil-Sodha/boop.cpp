8bf5b805f7e9703c369f7293b1350101d69cf467
Sodha Sohil J <161869362+Sohil-Sodha@users.noreply.github.com>
2024-04-14 08:54:43 +0530

    Update practical (2)(4).cpp

--- practical (2)(4).cpp
+++ practical (2)(4).cpp
@@ -2,41 +2,30 @@
 
 using namespace std;
 
-void sortArray(int arr[], int size) {
-
-    for (int i = 0; i < size - 1; ++i) {
-    
-        for (int j = 0; j < size - i - 1; ++j) {
-        
-            if (arr[j] > arr[j + 1]) {
-            
-                int temp = arr[j];
-                
-                arr[j] = arr[j + 1];
-                
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
 int main() {
     int numbers[10];
     int sum = 0;
 
-    cout << " enter 10 numbers: " << endl;
-
+    cout << "Enter 10 numbers:" << endl;
     for (int i = 0; i < 10; ++i) {
         cin >> numbers[i];
         sum += numbers[i];
     }
 
-    double average = static_cast<double>(sum) / 10;
+    double average = sum / 10.0;
+
+    for (int i = 0; i < 9; ++i) {
+        for (int j = i + 1; j < 10; ++j) {
+            if (numbers[i] > numbers[j]) {
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
 
     cout << "Sum: " << sum << endl;
     cout << "Average: " << average << endl;
-
-    sortArray(numbers, 10);
 
     cout << "Sorted array in ascending order:" << endl;
     for (int i = 0; i < 10; ++i) {
