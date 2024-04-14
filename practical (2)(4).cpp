21ed53157790acef04e019693cfe50677dca15c3
Sodha Sohil J <161869362+Sohil-Sodha@users.noreply.github.com>
2024-04-14 08:47:52 +0530

    Update practical (2)(4).cpp

--- practical (2)(4).cpp
+++ practical (2)(4).cpp
@@ -1,35 +1,48 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int x[100],n,i,j,temp;
-    cout<<"Enter The Limit : ";
-    cin>>n;
- 
-    cout<<"Enter "<<n<<" Value :"<<endl;
-    for(i=0;i<n;i++)
-    {
-        cin>>x[i];
-    }
- 
-    for(i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(x[i]>x[j])
-            {
-                temp=x[i];
-                x[i]=x[j];
-                x[j]=temp;
+void sortArray(int arr[], int size) {
+
+    for (int i = 0; i < size - 1; ++i) {
+    
+        for (int j = 0; j < size - i - 1; ++j) {
+        
+            if (arr[j] > arr[j + 1]) {
+            
+                int temp = arr[j];
+                
+                arr[j] = arr[j + 1];
+                
+                arr[j + 1] = temp;
             }
         }
     }
- 
-    cout<<"Sorted Array is : "<<endl;
-    for(i=0;i<n;i++)
-    {
-        cout<<x[i]<<endl;
+}
+
+int main() {
+    int numbers[10];
+    int sum = 0;
+
+    cout << " enter 10 numbers: " << endl;
+
+    for (int i = 0; i < 10; ++i) {
+        cin >> numbers[i];
+        sum += numbers[i];
     }
+
+    double average = static_cast<double>(sum) / 10;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Average: " << average << endl;
+
+    sortArray(numbers, 10);
+
+    cout << "Sorted array in ascending order:" << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
